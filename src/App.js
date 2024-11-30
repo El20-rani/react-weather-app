167f3b2b7e8ba05abf3d034a723eabf2fbefbba6
Eleni Zewdu <81524686+El20-rani@users.noreply.github.com>
2024-12-01 00:09:20 +0100

    removed unncessary files and added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+      </div>
+    </div>
+  );
 }
-
-export default App;
