e0479de420d14aa40e0e63e8d194eb2e7be15b16
Eleni Zewdu <81524686+El20-rani@users.noreply.github.com>
2024-12-01 00:47:20 +0100

    made the html frame for a weather app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from "react";
 import "./App.css";
+import Weather from "./weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
+        <Weather />
       </div>
     </div>
   );
