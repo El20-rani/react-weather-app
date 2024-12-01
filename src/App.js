e385897b24f1b7893e377223778c3d80beba43cb
Eleni Zewdu <81524686+El20-rani@users.noreply.github.com>
2024-12-01 01:52:17 +0100

    added some basic styling

--- src/App.js
+++ src/App.js
@@ -8,6 +8,18 @@ export default function App() {
       <div className="container">
         <h1>Weather App</h1>
         <Weather />
+        <footer>
+          <p>
+            This project was coded by Eleni Z. B. and is open-sourced
+            open-sourced on
+            <a
+              href="https://github.com/El20-rani/react-weather-app.git"
+              target="_blank"
+            >
+              GitHub
+            </a>
+          </p>
+        </footer>
       </div>
     </div>
   );
